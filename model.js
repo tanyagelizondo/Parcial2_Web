e7cec5b5ac9c169426c131a1cdd0d86cdb82148d
[tanyagelizondo] <tanya_yaretzi@live.com.mx>
2020-10-26 18:50:23 -0600

    ya jala el server

---
+++ model.js
@@ -0,0 +1,48 @@
+let mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+let userSchema = mongoose.Schema({
+    name: { type: String, require: true },
+    jobTitle: {type:String},
+    jobDescription: {type: String},
+    startDate: {type: String},
+    endDate: {type: String}
+});
+
+let User = mongoose.model('data', userSchema);
+
+let UserList = {
+    get: function (name) {
+        if (name) {
+            return User.findOne({ name: name })
+                .then(user => {
+                    return user;
+                })
+                .catch(error => {
+                    throw Error(error);
+                });
+        } else {
+            return User.find()
+                .then(users => {
+                    return users;
+                })
+                .catch(error => {
+                    throw Error(error);
+                });
+        }
+
+    },
+    
+//este es el post
+    register: function(newUser) {
+        return User.create(newUser)
+            .then(user => {
+                return user;
+            })
+            .catch(err => {
+                throw Error(err);
+            });
+    }
+};
+
+module.exports = {UserList};
--- server.js
+++ server.js
@@ -1,16 +1,108 @@
-const express = require("express");
-const port = 3000;
+let express = require('express');
+let bp = require('body-parser');
+let jsonParser = bp.json();
 
-const app = express()
+let mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+let {UserList} = require('./model');
 
-const db = require("./connections")
+let app = express();
 
-app.use(express.json())
+app.use(express.static('public'));
 
-const infoRouter = require("./info");
 
-app.use("/information",infoRouter);
+//Aquí van los end points
 
-app.listen(port, () =>
-    console.log(`Server running at http://localhost:${port}`)
-);
+app.post("/registerWorkplace",jsonParser,(req,res) => {
+    let { name,jobTitle,jobDescription,startDate, endDate} = req.body;
+
+    let newUser = {name,jobTitle,jobDescription,startDate, endDate};
+
+    UserList.register(newUser)
+    .then (
+        user => {
+            return res.status(201).json(user);
+        
+        }
+
+    ) .catch(
+        error => {
+            res.statusMessage = "Something went wrong with the DB";
+            return res.status(500).json({
+                message: "Something went wrong with the DB",
+                status: 500
+            })
+        }
+    )
+
+
+})
+
+
+app.get("/getWorkplace/:name",jsonParser,(req,res) => {
+    UserList.get()
+    .then (
+        user => {
+            return res.status(200).json(user);
+        
+        }
+
+    ) .catch(
+        error => {
+            res.statusMessage = "Something went wrong with the DB";
+            return res.status(500).json({
+                message: "Something went wrong with the DB",
+                status: 500
+            })
+        }
+    )
+
+
+})
+
+
+let server;
+
+function runServer(port, databaseUrl) {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(databaseUrl, response => {
+            if (response) {
+                return reject(response);
+            }
+            else {
+                server = app.listen(port, () => {
+                    console.log("App is running on port " + port);
+                    resolve();
+                })
+                    .on('error', err => {
+                        mongoose.disconnect();
+                        return reject(err);
+                    })
+            }
+        });
+    });
+}
+
+function closeServer() {
+    return mongoose.disconnect()
+        .then(() => {
+            return new Promise((resolve, reject) => {
+                console.log('Closing the server');
+                server.close(err => {
+                    if (err) {
+                        return reject(err);
+                    }
+                    else {
+                        resolve();
+                    }
+                });
+            });
+        });
+}
+
+runServer(3000, "mongodb://localhost/mydb")
+    .catch(err => {
+        console.log(err);
+    });
+
+module.exports = { app, runServer, closeServer };
